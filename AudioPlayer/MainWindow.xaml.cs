a9983917987082aebd3065ddd5672ed6390df236
arthur <arthurfoadjo@yahoo.com>
2022-06-23 16:53:21 +0200

    function to read a file implemented

--- AudioPlayer/MainWindow.xaml.cs
+++ AudioPlayer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,25 @@ namespace AudioPlayer
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MediaPlayer player = new MediaPlayer();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog(); // Opens the file explorer
+            fileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*"; // Filters the file types that the user can open, in this case he can only open mp3 files
+
+            // ShowDialog opens the dialog box,
+            // if it is open we open the file path, then we read the file
+            if (fileDialog.ShowDialog() == true)
+            {
+                player.Open(new System.Uri(fileDialog.FileName));
+                filePath.Text = fileDialog.FileName;
+                player.Play();               
+            }
         }
     }
 }
